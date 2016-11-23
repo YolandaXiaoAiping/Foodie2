706d609dcf90779483658605627c8a85e4f0544c
YolandaXiaoAiping <aiping.xiao@mail.utoronto.com>
2016-11-22 23:10:31 -0500

    1

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,17 @@ $("#homepage_btn").click(function(){
 			}
 		});
 });
+//the same for sign up button
+$("#sign_up_btn").click(function(){
+	$.ajax({
+			url:"/signup",
+			type:"GET",
+			success:function(response){
+				if(response == 'Err'){
+					alert('You have already signed in !');
+				}else{
+					window.location='/signup.html';
+				}
+			}
+		});
+});
--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -8,15 +8,29 @@ $(document).ready(function(){
 			//alert(name);
 			$("#Username").text(name);
 			$('.ui.dropdown').dropdown();
-
 		}
 	});
 });
 
 $("#mainpage_home").click(function(){
-	
+
 });
 
 $("#dashboard").click(function(){
 	window.location='/dashboard.html';
+});
+
+$("#view_profile").click(function(){
+	window.location = 'view_profile.html';
+});
+
+$("#sign_out").click(function(){
+	$.ajax({
+		url:"/signout",
+		type:"GET",
+		success:function(){
+			window.location = "index.html";
+		}
+
+	});
 });
